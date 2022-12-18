b4b1f230e7208cd0afd509b31269b9ed8d0b7f18
Chen_TianHe <yangchen6319@email.com>
2022-12-18 22:18:59 +0800

    原型方法

---
+++ prototype/src/Client.java
@@ -0,0 +1,18 @@
+public class Client {
+
+    public static void main(String[] args) {
+
+        Sheep sheep1 = new Sheep("Tom", 12, "white");
+        Sheep sheep2 = (Sheep) sheep1.clone();
+        Sheep sheep3 = (Sheep) sheep1.clone();
+        Sheep sheep4 = (Sheep) sheep1.clone();
+        Sheep sheep5 = (Sheep) sheep1.clone();
+
+        System.out.println(sheep1);
+        System.out.println(sheep2);
+        System.out.println(sheep3);
+        System.out.println(sheep4);
+        System.out.println(sheep5);
+
+    }
+}
